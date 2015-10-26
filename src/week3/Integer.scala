52526ee6788a5c193fee643ce93cbe99d0f18d8c
Danny van Dam <d.van.dam@vnumedia.nl>
2015-10-26 15:11:24 +0100

    Play around with method names

---
+++ src/week3/Integer.scala
@@ -0,0 +1,18 @@
+package week3
+
+class Integer (val value: Int) {
+  def + (other: Integer) = new Integer(value + other.value)
+  def - (other: Integer) = new Integer(value - other.value)
+  def / (other: Integer) = new Integer(Math.round(value / other.value))
+  def * (other: Integer) = new Integer(value * other.value)
+
+  override def toString = value.toString
+}
+
+object Integer extends App {
+  def apply(value: Int) = new Integer(value)
+
+  println(Integer(18) + Integer(6)  / Integer(3)  - Integer(6)  * Integer(2))
+
+  println(Integer(18).+(Integer(6))./(Integer(3)).-(Integer(6)).*(Integer(2)))
+}
