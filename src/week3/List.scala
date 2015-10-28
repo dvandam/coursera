54f328e4ca70fb37b7525fb5c0c4041afc2858d0
Danny van Dam <d.van.dam@vnumedia.nl>
2015-10-28 20:59:20 +0100

    Add covariance to List, so Nil can be an object

--- src/week3/List.scala
+++ src/week3/List.scala
@@ -2,30 +2,34 @@ package week3
 
 import java.util.NoSuchElementException
 
-trait List[T] {
+trait List[+T] {
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
 }
 
 object List {
-  def apply[T](): List[T] = new Nil
-  def apply[T](x: T): List[T] = new Cons(x, new Nil)
-  def apply[T](x: T, y: T): List[T] = new Cons(x, new Cons(y, new Nil))
+  def apply[T](): List[T] = Nil
+  def apply[T](x: T): List[T] = new Cons(x, Nil)
+  def apply[T](x: T, y: T): List[T] = new Cons(x, new Cons(y, Nil))
 }
 
 class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty = false
 }
 
-class Nil[T] extends List[T] {
+object Nil extends List[Nothing] {
   def isEmpty = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
 
+object test {
+  val x: List[String] = Nil
+}
+
 object nth extends App {
-  def singleton[T](elem: T) = new Cons[T](elem, new Nil[T])
+  def singleton[T](elem: T) = new Cons[T](elem, Nil)
 
   singleton[Int](1)
   singleton[Boolean](true)
@@ -39,7 +43,7 @@ object nth extends App {
     else if (n == 0) xs.head
     else nth(n - 1, xs.tail)
 
-  val list = new Cons(1, new Cons(2, new Cons(3, new Nil)))
+  val list = new Cons(1, new Cons(2, new Cons(3, Nil)))
   println(nth(2, list))
   println(nth(0, list))
 }
