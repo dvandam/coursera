604850684b482433bd0242f62866038be0ff74d1
Danny van Dam <d.van.dam@vnumedia.nl>
2015-10-29 10:57:06 +0100

    Normal integers already have this behavior

--- src/week3/Integer.scala
+++ src/week3/Integer.scala
@@ -12,7 +12,10 @@ class Integer (val value: Int) {
 object Integer extends App {
   def apply(value: Int) = new Integer(value)
 
-  println(Integer(18) + Integer(6)  / Integer(3)  - Integer(6)  * Integer(2))
+//  println(Integer(18) + Integer(6)  / Integer(3)  - Integer(6)  * Integer(2))
+//  println(Integer(18).+(Integer(6))./(Integer(3)).-(Integer(6)).*(Integer(2)))
 
-  println(Integer(18).+(Integer(6))./(Integer(3)).-(Integer(6)).*(Integer(2)))
+  println(18  + 6  / 3  - 6  * 2)
+  println(18 .+(6)./(3).-(6).*(2))
+
 }
