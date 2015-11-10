de353503f7fd7567e3b8f4b9c5c5091737b860d7
Danny van Dam <d.van.dam@vnumedia.nl>
2015-11-10 11:56:00 +0100

    Working

--- src/week4/Expr.scala
+++ src/week4/Expr.scala
@@ -1,25 +1,30 @@
 package week4
-//
-//trait Expr {
-//  def isNumber: Boolean
-//  def isSum: Boolean
-//  def numValue: Int
-//  def leftOp: Expr
-//  def rightOp: Expr
-//}
-//
-//class Number(n: Int) extends Expr {
-//  def isNumber = true
-//  def isSum = false
-//  def numValue = n
-//  def leftOp = throw new Error("Number.leftOp")
-//  def rightOp = throw new Error("Number.rightOp")
-//}
-//
-//class Sum(e1: Expr, e2: Expr) extends Expr {
-//  def isNumber = false
-//  def isSum = true
-//  def numValue = throw new Error("Sum.numValue")
-//  def leftOp = e1
-//  def rightOp = e2
-//}
+
+trait Expr {
+  def eval: Int = this match {
+    case Number(n) => n
+    case Sum(e1, e2) => e1.eval + e2.eval
+    case Prod(e1, e2) => e1.eval * e2.eval
+  }
+  def show: String = this match {
+    case Number(n) => n.toString
+    case Sum(e1, e2) => e1.show + " + " + e2.show
+    case Prod(e1:Sum, e2:Sum) => "(" + e1.show + ") * (" + e2.show + ")"
+    case Prod(e1, e2:Sum) => e1.show + " * (" + e2.show + ")"
+    case Prod(e1:Sum, e2) => "(" + e1.show + ") * " + e2.show
+    case Prod(e1, e2) => e1.show + " * " + e2.show
+    case Var(x) => x
+  }
+}
+
+object expr extends App {
+  println(Sum(Number(4), Number(40)).show)
+  println(Sum(Number(4), Number(40)).eval)
+  println(Sum(Prod(Number(2), Var("x")), Var("y")).show)
+  println(Prod(Sum(Number(2), Var("x")), Var("y")).show)
+}
+
+case class Number(n: Int) extends Expr
+case class Sum(e1: Expr, e2: Expr) extends Expr
+case class Prod(e1: Expr, e2: Expr) extends Expr
+case class Var(x: String) extends Expr
